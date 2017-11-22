211d7c76b5fb68701443cb03e5938f596b7d2957
SarahPrettner <sarah_prettner@hotmail.com>
2017-11-22 20:55:34 +0100

    added content of template first draft

--- chef_repo/topic2/templates/motd.erb.rb
+++ chef_repo/topic2/templates/motd.erb.rb
@@ -1,40 +1,13 @@
 #
 # Cookbook:: topic2
-# Recipe:: default
+# Template for message of the day
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-execute "update-upgrade" do
-	command "apt-get update && apt-get upgrade -y"
-	acrion :run
-end
-
-package 'openssh' do
-	    action :upgrade
-end
-
-package 'vim' 'nano' 'git' 'vi' 'tree' 'rsyslog' do
-	    action :upgrade
-end
-
-template '/etc/motd' do
-	source 'motd.erb'
-	owner 'root'
-	group 'root'
-        mode '0755'
-end
-
-package 'nginx' do
-	    version '1.12.2'
-end
-
-user 'ckappel' do
-	home '/home/linuxguru'
-	shell '/bin/bash'
-	password
-end
-
-service "nginx" do
-	action :start
-end
-
+<%= @group %>
+<% @members.each do |item| %>
+	- <%= item %>
+<% end %>
+<%= @lecturer %>
+# missing newdate command
+<%= @message%>
