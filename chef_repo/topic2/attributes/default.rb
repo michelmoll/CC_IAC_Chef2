dc107a400fc363c352219374f8b1d4e92d24306d
Michel Moll <wi15b077@technikum-wien.at>
2017-11-26 23:34:02 +0100

    Added log file

--- chef_repo/topic2/attributes/default.rb
+++ chef_repo/topic2/attributes/default.rb
@@ -6,7 +6,7 @@ default['nginx']['port']         = ['8080','8443']
 default['nginx']['default_root'] = '/var/www/nginx-default'
 
 #define attributes for the template motd
-default['motd']['group'] = 'Group2'
+default['motd']['group'] = 'GROUP2'
 default['motd']['members'] = ['MOLL','PRETTNER','WÜST']
 default['motd']['lecturer'] = ['KAPPEL','CHRISTOPH']
 default['motd']['message'] = 'Change me'
